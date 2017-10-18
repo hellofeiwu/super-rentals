6f5fcfde81da0c062f2cbde4977ce699ad545566
Wu <FWU31@ford.com>
2017-10-18 17:03:27 +0800

    add controller

--- app/helpers/rental-property-type.js
+++ app/helpers/rental-property-type.js
@@ -1,7 +1,16 @@
 import Ember from 'ember';
 
-export function rentalPropertyType(params/*, hash*/) {
-  return params;
+const communityPropertyTypes = [
+  'Condo',
+  'Townhouse',
+  'Apartment'
+];
+
+export function rentalPropertyType([propertyType]) {
+  if (communityPropertyTypes.includes(propertyType)) {
+    return 'Community';
+  }
+  return 'Standalone';
 }
 
 export default Ember.Helper.helper(rentalPropertyType);
